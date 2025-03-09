e2d8edb7aebd0339417c6f36a2f2d7edbdeb4b94
reazulhasan44 <reazshovon44@gmail.com>
2025-03-10 00:09:11 +0600

    Procuct microservice

--- Source/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ Source/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Catalog.API.Products.GetProductById
+﻿using Catalog.API.Exceptions;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById
 {
-    public class GetProductByIdHandler
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
     }
 }
