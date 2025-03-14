d52b503acfbef99b129d2e6b5312c6769f97c35d
reazulhasan44 <reazshovon44@gmail.com>
2025-03-15 01:47:30 +0600

    project configure using carter and cqrs

--- Source/Services/Basket/Basket.API/GlobalUsing.cs
+++ Source/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,1 +1,8 @@
 ﻿global using BuildingBlocks.Exceptions;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.CQRS;
--- Source/Services/Basket/Basket.API/Program.cs
+++ Source/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,26 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 
 app.Run();
