c5c7b7dc3789bab73cf2ca38020a1d57cd120c0e
reazulhasan44 <reazshovon44@gmail.com>
2025-03-17 02:09:16 +0600

    Basket api docker orchestra done

--- Source/Services/Basket/Basket.API/Program.cs
+++ Source/Services/Basket/Basket.API/Program.cs
@@ -38,7 +38,8 @@ builder.Services.AddStackExchangeRedisCache(options =>
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
-    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
