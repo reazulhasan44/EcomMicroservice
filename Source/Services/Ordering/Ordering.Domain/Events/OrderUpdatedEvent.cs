6200b4b18c06fdd440a03d95e7a4e5b3b1735246
reazulhasan44 <reazshovon44@gmail.com>
2025-03-23 09:21:57 +0600

    global

--- Source/Services/Ordering/Ordering.Domain/Events/OrderUpdatedEvent.cs
+++ Source/Services/Ordering/Ordering.Domain/Events/OrderUpdatedEvent.cs
@@ -1,9 +1,4 @@
-﻿using Ordering.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Ordering.Domain.Events
 {
--- Source/Services/Ordering/Ordering.Domain/GlobalUsing.cs
+++ Source/Services/Ordering/Ordering.Domain/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using Ordering.Domain.Abstractions;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.Events;
+global using Ordering.Domain.Models;
