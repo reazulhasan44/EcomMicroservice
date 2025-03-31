a551ed22fed77e97017610300f2ff5fc9270ff2a
reazulhasan44 <reazshovon44@gmail.com>
2025-04-01 03:04:16 +0600

    dto , and feature created for create Order

---
+++ Source/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
