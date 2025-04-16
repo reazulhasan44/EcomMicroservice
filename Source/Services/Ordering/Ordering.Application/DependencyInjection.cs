7f5fe265710debe0e07e188487cb3c8858b60bce
reazulhasan44 <reazshovon44@gmail.com>
2025-04-16 22:31:07 +0600

    Integration Event Basket Checkout from Ordering Application

--- Source/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ Source/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -20,6 +21,7 @@ namespace Ordering.Application
                 config.AddOpenBehavior(typeof(ValidationBehavior<,>));
                 config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
+            services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
             return services; 
         }   
     }
